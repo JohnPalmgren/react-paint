b4481fd8a21dc4c81dfce5707ded3bc6c1682c44
John <johnmpalmgren@gmail.com>
2022-03-14 17:03:36 +0000

    Context component added

---
+++ src/components/Canvas.js
@@ -0,0 +1,67 @@
+import "../App.css";
+import { useEffect, useRef, useState } from "react";
+
+const Canvas = () => {
+  const canvasRef = useRef(null);
+  const contextRef = useRef(null);
+
+  // state set when mouse is pressed and removed when mouse is released
+  const [isDrawing, setIsDrawing] = useState("false");
+
+
+  // useEffect initialises the default canvas styles when the component first runs
+  // Add color and line width here from buttons (through props) and add as dependencies
+  useEffect(() => {
+    const canvas = canvasRef.current;
+    const ctx = canvas.getContext("2d");
+    ctx.lineCap = "round";
+    ctx.lineJoin = "round";
+    contextRef.current = ctx;
+  }, []);
+
+  // function runs when mouse button is pressed.
+  // updates canvas context to set position to be drawn on
+  const startDrawing = (event) => {
+    contextRef.current.beginPath();
+    contextRef.current.moveTo(
+      event.nativeEvent.offsetX,
+      event.nativeEvent.offsetY
+    );
+    setIsDrawing(true);
+  };
+
+  // function runs when mouse button is released.
+  // updates canvas context to stop drawing
+  const endDrawing = () => {
+    contextRef.current.closePath();
+    setIsDrawing(false);
+  };
+
+  // function runs while mouse is moving. If isDrawing state is false function returns without making any change
+  // else canvas context is updated to fill in a line on the current point on the page
+  const draw = (event) => {
+    if (!isDrawing) {
+      return;
+    }
+    contextRef.current.lineTo(
+      event.nativeEvent.offsetX,
+      event.nativeEvent.offsetY
+    );
+
+    contextRef.current.stroke();
+  };
+
+  return (
+    <canvas
+      className={"canvas"}
+      onMouseDown={startDrawing}
+      onMouseUp={endDrawing}
+      onMouseMove={draw}
+      ref={canvasRef}
+      width={"500px"}
+      height={"500px"}
+    />
+  );
+};
+
+export default Canvas;
