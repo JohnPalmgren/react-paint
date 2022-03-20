b0f1bd3d912f9db1eac749956709240c6491428d
Ross Stevenson <Ross.w.Stevenson@gmail.com>
2022-03-20 13:23:36 +0000

    added responsive media queries. tested them with dev tools and working fine but live testing results differed

--- src/components/Heading.js
+++ src/components/Heading.js
@@ -3,7 +3,6 @@ const Heading = () => {
     <>
       <div className="headingWrapper">
         <div className="graphic-container">
-        {/* <div className="image"></div> */}
         <h1 className="headingH1">Paint Splash</h1>
         </div>
       </div>
