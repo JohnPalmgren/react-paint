27c48a27b33577cac3edc3ac86bf96d69c437742
Ross Stevenson <Ross.w.Stevenson@gmail.com>
2022-03-20 12:10:51 +0000

    Changed the header structure to help responsiveness. Instead of having a container with the splash image and the text. I set the splash image as the background of the div then set the text inside the div.

--- src/components/Heading.js
+++ src/components/Heading.js
@@ -2,9 +2,10 @@ const Heading = () => {
   return (
     <>
       <div className="headingWrapper">
-        
-        <div className="image"></div>
+        <div className="graphic-container">
+        {/* <div className="image"></div> */}
         <h1 className="headingH1">Paint Splash</h1>
+        </div>
       </div>
     </>
   );
